a3eabe40736c73348cc6b5183c91df27495202f0
masatana <plaza.tumbling@gmail.com>
2014-09-21 06:45:40 -0700

    remove o-

--- k-db.py
+++ k-db.py
@@ -21,35 +21,44 @@ def format_date(x, pos=None, N=None, r=None):
     thisind = np.clip(int(x+0.5), 0, N-1)
     return r.date[thisind].strftime("%Y-%m-%d")
 
+def format_response(response):
+    return "\n".join(
+            response
+            .read()
+            .decode(encoding="Shift-JIS")
+            .replace("\r\n", "\n")
+            .strip()
+            .split()[2:])
+
 def crawl(ticker_symbols):
     for code, name in ticker_symbols.items():
-        for year in range(2007, 2014):
-            with open("./csv/{year}_{name}.csv".format(year = year, name = name), "w") as f:
-                response = urllib.request.urlopen(STOCK_URL.format(code = code, year = year))
-                f.write(response.read().decode(encoding="Shift-JIS").replace("\r\n", "\n"))
+        with open("./csv/{code}.csv".format(code=code), "w") as f:
+            for year in range(2007, 2014):
+                response = urllib.request.urlopen(STOCK_URL.format(code=code, year=year))
+                f.write(format_response(response))
             time.sleep(1)
 
     # NIKKEI STOCK AVERAGE
-    for year in range(2007, 2014):
-        with open("./csv/{year}_NIKKEI.csv".format(year = year), "w") as f:
-            response = urllib.request.urlopen(NIKKEI_URL.format(code = code, year = year))
-            f.write(response.read().decode(encoding="Shift-JIS").replace("\r\n", "\n"))
+    with open("./csv/NIKKEI.csv", "w") as f:
+        for year in range(2007, 2014):
+            response = urllib.request.urlopen(NIKKEI_URL.format(code=code, year=year))
+            f.write(format_response(response))
         time.sleep(1)
 
 def analyze(ticker_symbols):
-    col_names = ("date", "initial", "high", "low", "end",)
-    for code, name in ticker_symbols.items():
-        for year in range(2007, 2014):
-            dat = mlab.csv2rec("./csv/{year}_{name}.csv".format(year=year, name=name), skiprows=2, names=col_names)
-            N = len(dat)
-            ind = np.arange(N)
-            fig = plt.figure()
-            ax = fig.add_subplot(111)
-            ax.plot(ind, dat.end, "o-")
-            applied_format_date = partial(format_date, N=N, r=dat)
-            ax.xaxis.set_major_formatter(ticker.FuncFormatter(applied_format_date))
-            fig.autofmt_xdate()
-            plt.savefig("./image_{code}_{year}.png".format(code=code, year=year))
+    col_names = ("date", "initial", "high", "low", "end", "volume", "sales_value",)
+    for code in ticker_symbols:
+        print(code)
+        dat = mlab.csv2rec("./csv/{code}.csv".format(code=code), skiprows=2, names=col_names)
+        N = len(dat)
+        ind = np.arange(N)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(ind, dat.end)
+        applied_format_date = partial(format_date, N=N, r=dat)
+        ax.xaxis.set_major_formatter(ticker.FuncFormatter(applied_format_date))
+        fig.autofmt_xdate()
+        plt.savefig("./image_{code}.png".format(code=code))
 
 if __name__ == "__main__":
     with open("./ticker_symbol.list") as f:
